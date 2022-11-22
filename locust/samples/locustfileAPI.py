8a5886dfdde2d3e2661dc431108075b526bc4b1f
Fumihiro_Maejima <testerwatson1101@gmail.com>
2022-11-23 00:44:27 +0900

    add min_wait,max_wait.

--- locust/samples/locustfileAPI.py
+++ locust/samples/locustfileAPI.py
@@ -1,6 +1,10 @@
 from locust import HttpUser, task
 
 class DebugUser(HttpUser):
+    # 2〜5秒間隔で各種テストを実行
+    min_wait = 2000 # 待ち時間の最小(ms)
+    max_wait = 5000 # 待ち時間の最大(ms)
+
     @task
     def debug_test(self):
         self.client.get("/api/v1/debug/test")
